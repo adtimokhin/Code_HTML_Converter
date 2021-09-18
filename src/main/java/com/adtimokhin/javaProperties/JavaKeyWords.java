e85ca1e2a0fcfdfbe34939251ec7c1d1d2513355
adtimokhin <adtimokhin@gmail.com>
2021-09-19 00:44:36 +0100

    .

--- src/main/java/com/adtimokhin/javaProperties/JavaKeyWords.java
+++ src/main/java/com/adtimokhin/javaProperties/JavaKeyWords.java
@@ -10,18 +10,17 @@ public class JavaKeyWords {
      */
     private static final String[] keyWords = {"public", "private", "protected",
             "this", "return", "class", "interface", "int", "void",
-            "float", "boolean", "true", "false" , "extends" , "implements",
+            "float", "boolean", "true", "false", "extends", "implements",
             "null", "new", "if", "else", "static", "for"};
 
 
     /**
      * Method that checks if {@code word} is a keyword.
      *
-     *
      * @param word a word that needs to be compared to check if it is a keyword or not.
      * @return true if the {@code word} is in {@link #keyWords} array.
      */
-    public static boolean isKeyWord(String word){
+    public static boolean isKeyWord(String word) {
         for (String keyWord : keyWords) {
             if (keyWord.equals(word)) {
                 return true;
